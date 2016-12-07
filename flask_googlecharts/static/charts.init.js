913823684fedce20d9be69e2e50167318ce4f54f
Kevin Schellenberg <wikkiewikkie@gmail.com>
2016-12-07 13:09:00 -0500

    draw charts with url data sources asynchronously

--- flask_googlecharts/static/charts.init.js
+++ flask_googlecharts/static/charts.init.js
@@ -2,35 +2,25 @@ function drawCharts() {
 
     $('.chart').each( function(i, c) {
 
+        var chartName = c.getAttribute('data-chart-name');
         var chartType = c.getAttribute("data-chart-type");
         var chartObject = new google[c.getAttribute("data-chart-class")][chartType](c);
-        var chartName = c.getAttribute('data-chart-name');
-        var chartOptions = getChartOptions(chartName);
-        var chartData = getChartData(c);
-        if (c.getAttribute('data-chart-material')) {;
-            chartObject.draw(chartData, google.charts[chartType].convertOptions(chartOptions));
-        } else {
-            chartObject.draw(chartData, chartOptions);
+        var chartOptions = JSON.parse($('#chart-options-' + chartName).text());
+        var chartDataUrl = c.getAttribute('data-chart-data-url');
+
+        if (c.getAttribute('data-chart-material')) {
+            chartOptions = google.charts[chartType].convertOptions(chartOptions);
+        }
+
+        if (chartDataUrl) {
+            chartObject.options = chartOptions;
+            $.ajax({url: chartDataUrl,
+                    context: chartObject}).done( function(d) {this.draw(new google.visualization.DataTable(d)); } );
+        } else if (c.getAttribute('data-chart-data-element')) {
+            chartObject.draw(new google.visualization.DataTable(JSON.parse($('#chart-data-' + chartName).text())),
+                             chartOptions);
         }
 
     } );
-
-}
-
-function getChartOptions(n) {
-    return JSON.parse($('#chart-options-' + n).text());
-}
-
-function getChartData(c) {
-  var data = null;
-  if ( c.getAttribute('data-chart-data-element') ) {
-        data = JSON.parse($('#chart-data-' + c.getAttribute('data-chart-name')).text());
-  } else if ( c.getAttribute('data-chart-data-url') ) {
-        data = $.ajax({url: c.getAttribute('data-chart-data-url'), dataType: 'json', async: false}).responseText;
-  }
-
-  if (data) {
-    return new google.visualization.DataTable(data);
-  }
 
 }
